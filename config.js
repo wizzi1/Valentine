fa1dc33868583f4f6b936e8d611475eeb37db73d
wizzi1 <tudosobregame855@gmail.com>
2026-02-02 15:42:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dgyvysifw/video/upload/v1770063119/Get_You_-_Daniel_Caesar_feat._Kali_Uchis_meyn2d.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dgyvysifw/video/upload/v1770064764/get-you-daniel-caesar-feat-kali-uchis_GEPJWgH1_u94xpn.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
